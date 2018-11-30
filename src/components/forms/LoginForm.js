fcc66e3248d5ebe27a4de7f18a59e95a402c6f48
Cristhian Valencia <valenciacristhian47@gmail.com>
2018-11-30 18:59:06 -0300

    partial form

---
+++ src/components/forms/LoginForm.js
@@ -0,0 +1,73 @@
+import React, { Component } from "react";
+import { Form, Button } from "semantic-ui-react";
+import Validator from "validator";
+import InlineError from "../messages/InlineError";
+
+class LoginForm extends Component {
+  state = {
+    data: {
+      email: "",
+      password: ""
+    },
+    loading: false,
+    errors: {}
+  };
+
+  onChange = e => {
+    this.setState({
+      data: { ...this.state.data, [e.target.name]: e.target.value }
+    });
+  };
+
+  onSubmit = () => {
+    const errors = this.validate(this.state.data);
+    this.setState({ errors });
+  };
+
+  validate = data => {
+    const errors = {};
+    if (!Validator.isEmail(data.email)) {
+      errors.email = "Please enter a valid email";
+    }
+    if (!data.password) {
+      errors.password = "Password can't be empty";
+    }
+
+    return errors;
+  };
+
+  render() {
+    const { data, errors } = this.state;
+    return (
+      <Form onSubmit={this.onSubmit}>
+        <Form.Field error={!!errors.email}>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="example@example.com"
+            value={data.email}
+            onChange={this.onChange}
+          />
+          {errors.email && <InlineError text={errors.email} />}
+        </Form.Field>
+        <Form.Field error={!!errors.password}>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Secure password"
+            value={data.password}
+            onChange={this.onChange}
+          />
+          {errors.password && <InlineError text={errors.password} />}
+        </Form.Field>
+        <Button primary>Login</Button>
+      </Form>
+    );
+  }
+}
+
+export default LoginForm;
--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,5 +1,11 @@
 import React from "react";
+import LoginForm from "../forms/LoginForm";
 
-const LoginPage = () => <h1>Login page</h1>;
+const LoginPage = () => (
+  <div>
+    <h1>Login page</h1>
+    <LoginForm />
+  </div>
+);
 
 export default LoginPage;
